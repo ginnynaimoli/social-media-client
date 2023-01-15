cdfa08ca08bda8989c3066fc77c57b174e9072ca
ginnynaimoli <58743410+ginnynaimoli@users.noreply.github.com>
2023-01-15 01:22:18 -0500

    Update UserInfo.jsx

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -104,7 +104,7 @@ const UserInfo = ({ userId, picturePath }) => {
 
         <FlexBetween gap="1rem">
           <FlexBetween gap="1rem">
-            <img src="../assets/twitter.png" alt="linkedin" />
+            <img src="../assets/linkedin.png" alt="linkedin" />
             <Box>
               <Typography color={main} fontWeight="500">
                 LinkedIn
